7e971d8e89704881568c8e198a2075ee3bae60f1
DarioDistaso <dariodistaso@homtail.com>
2021-03-03 18:10:45 -0300

    sa 5 alterada

--- sa5etapa2.py
+++ sa5etapa2.py
@@ -6,12 +6,12 @@ Resultados esperados: Código fonte com as implementações solicitadas. Critér
 
 
 def mergesort(lista):
-    if len(lista) > 1: # vai parar a divisão até que tiver um só elemento na lista
+    if len(lista) > 1: # condição de parada: vai parar a divisão até que tiver um só elemento na lista
         meio = len(lista) // 2
         esquerda = lista[:meio] # divide a lista do inicio até o meio e chamamos essa metade de esquerda
         direita = lista[meio:] # divide a lista do meio até o fim e chamamos essa metade de direita
-        mergesort(esquerda)
-        mergesort(direita)
+        mergesort(esquerda) # chamada recursiva da função mergesort
+        mergesort(direita) # chamada recursiva da função mergesort
         
         i = 0 # indice da lista de esquerda
         j = 0 # indice da lista de direita
@@ -20,20 +20,20 @@ def mergesort(lista):
         while i < len(esquerda) and j < len(direita): # os indices i e j serão sempre menores do tamanho das sub-listas
             if esquerda[i] < direita[j]:
                 lista[k] = esquerda[i] # coloca o numero menor na lista original
-                i += 1 # incrementa o indice da esquerda
+                i += 1 # incrementa o indice da sub-lista esquerda
                 k += 1 # incrementa o indice da lista original
             else:
                 lista[k] = direita[j] # coloca o numero menor na lista original
-                j += 1 # incrementa o indice da direita
+                j += 1 # incrementa o indice da sub-lista direita
                 k += 1 # incrementa o indice da lista original
 
         while i < len(esquerda): # se algum numero da esquerda ficar para fora do primeiro loop será colocado na lista original
                 lista[k] = esquerda[i] # coloca o numero menor na lista original
-                i += 1 # incrementa o indice da esquerda
+                i += 1 # incrementa o indice da sub-lista esquerda
                 k += 1 # incrementa o indice da lista original
         while j < len(direita): # se algum numero da direita ficar para fora do primeiro loop será colocado na lista original
                 lista[k] = direita[j] # coloca o numero menor na lista original
-                j += 1 # incrementa o indice da direita
+                j += 1 # incrementa o indice da sub-lista direita
                 k += 1 # incrementa o indice da lista original
 
 lista = []
